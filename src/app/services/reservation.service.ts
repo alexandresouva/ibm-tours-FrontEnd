c3a346a5b475fcb3abf2a72cfb9d0997db9496d7
Alexandre Sousa Silva <pseudoalexandre@gmail.com>
2023-08-08 12:17:46 -0300

    refactor: adjust baseUrl to requests and remove test files

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -2,35 +2,36 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/Reservation';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReservationService {
-  private readonly API = 'http://localhost:8080/reservas';
+  private readonly baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) {}
 
   findAll(): Observable<Reservation[]> {
-    return this.http.get<Reservation[]>(this.API);
+    return this.http.get<Reservation[]>(this.baseUrl);
   }
 
   findById(id: number) {
-    const url = `${this.API}/${id}`;
+    const url = `${this.baseUrl}/${id}`;
     return this.http.get<Reservation>(url);
   }
 
   create(reservation: Reservation): Observable<Reservation> {
-    return this.http.post<Reservation>(this.API, reservation);
+    return this.http.post<Reservation>(this.baseUrl, reservation);
   }
 
   delete(id: number): Observable<Reservation> {
-    const url = `${this.API}/${id}/cancelar`;
+    const url = `${this.baseUrl}/${id}/cancelar`;
     return this.http.delete<Reservation>(url);
   }
 
   edit(reservation: Reservation): Observable<Reservation> {
-    const url = `${this.API}/${reservation.id}`;
+    const url = `${this.baseUrl}/${reservation.id}`;
     return this.http.put<Reservation>(url, reservation);
   }
 }
